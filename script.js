469c2bce4a8a0abd5fbee54b4a92019d0667045c
LeoMaDev <leonelfranco09@gmail.com>
2024-06-02 19:08:38 -0300

    Subo el desarrollo de una web sobre viajes hecha en el curso de Sergie Code

---
+++ script.js
@@ -0,0 +1,43 @@
+//Desestructuracion de datos
+import { barcelona, roma, paris, londres } from './ciudades.js'
+
+//Obtener los elementos del DOM
+let enlaces = document.querySelectorAll('a')
+let tituloElemento = document.getElementById('titulo')
+let subTituloElemento = document.getElementById('subtitulo')
+let parrafoElemento = document.getElementById('parrafo')
+let precioElemento = document.getElementById('precio')
+
+//Agregar un evemto CLICK a cada enlace
+enlaces.forEach(function (enlace) {
+    enlace.addEventListener('click', function () {
+        //Remover la clase "active" de todos los enlaces
+        enlaces.forEach(function (enlace) {
+            enlace.classList.remove('active')
+        });
+
+        //Agregar la clase "active" al enlace actual
+        this.classList.add('active')
+        
+        //Obtener el contenido correspondiente según el enlace
+        let contenido = obtenerContenido(this.textContent)
+
+        //Mostrar el contenido en el DOM
+        tituloElemento.innerHTML = contenido.titulo
+        subTituloElemento.innerHTML = contenido.subtitulo
+        parrafoElemento.innerHTML = contenido.parrafo
+        precioElemento.innerHTML = contenido.precio
+
+    })
+});
+
+//FUNCION PARA TRAER LA INFORMACION CORRECTA DESDE CIUDADES.JS
+function obtenerContenido(enlace) {
+    let contenido = {
+        'Barcelona': barcelona,
+        'Roma': roma,
+        'París': paris,
+        'Londres': londres,
+    };
+    return contenido[enlace];
+}
